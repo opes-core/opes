562690f4a3c086f3358206d183b4cbd8c6011147
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-12-22 22:01:04 +0530

    engineering boost
    - corrected sharpe/sortino edge cases
    - added extensive cost error handling

--- opes/backtester.py
+++ opes/backtester.py
@@ -1,3 +1,5 @@
+from numbers import Real
+
 import numpy as np
 import pandas as pd
 import scipy.stats as scistats
@@ -7,9 +9,11 @@ from opes.utils import slippage, extract_data
 
 class Backtester():
     
-    def __init__(self, train_data=None, test_data=None, cost={'const': 10}):
-        self.train = train_data
-        self.test = test_data
+    def __init__(self, train_data=None, test_data=None, cost={'const': 10.0}):
+        # Assigning by dropping nans to ensure proper indexing
+        # Dropping nan rows results makes backtest loops robust and predictable
+        self.train = train_data.dropna()
+        self.test = test_data.dropna()
         self.cost = cost
     
     def backtest_integrity_check(self, optimizer, rebalance_freq, seed):
@@ -27,15 +31,22 @@ class Backtester():
         # Training data validity
         if len(self.train) <= 5:
             raise PortfolioError(f"Training data insufficient. {len(self.train)} timesteps too short")
-        # Cost model validity
+        # Cost model validity - per model check
         if len(self.cost) != 1:
             raise PortfolioError(f"Invalid cost model. Cost model must be a dictionary of length 1, Got {len(self.cost)}")
         first_key = next(iter(self.cost))
-        if first_key.lower() not in ['const', 'lognormal', 'gamma', 'inversegaussian', 'jump']:
+        first_key_low = first_key.lower()
+        if first_key_low not in ['const', 'lognormal', 'gamma', 'inversegaussian', 'jump']:
             raise PortfolioError(f"Unknown cost model: {first_key}")
+        elif (first_key_low == 'const' and not isinstance(self.cost[first_key], Real)):
+            raise PortfolioError(f"Unspecified cost value. Expected real number, got {type(self.cost[first_key])}")
+        elif first_key_low in ['lognormal', 'gamma', 'inversegaussian'] and len(self.cost[first_key]) != 2:
+            raise PortfolioError(f"Invalid cost model parameter length. Expected 2, got {len(self.cost[first_key])}")
+        elif first_key_low == 'jump' and len(self.cost[first_key]) != 3:
+            raise PortfolioError(f"Invalid jump cost model parameter length. Expected 3, got {len(self.cost[first_key])}")
+            
 
     def backtest(self, optimizer, rebalance_freq=None, seed=None):
-        
         # Running backtester integrity checks
         self.backtest_integrity_check(optimizer, rebalance_freq, seed)
         # Backtest loop
@@ -60,14 +71,15 @@ class Backtester():
         return portfolio_returns
 
     def get_metrics(self, returns):
+        # Caching repeated values
         returns = np.array(returns)
         average = returns.mean()
         downside_vol = returns[returns < 0].std()
         vol = returns.std()
 
         # Performance metrics
-        SHARPE = np.sqrt(252) * average / vol if (vol > 0 or not np.isnan(vol)) else np.nan
-        SORTINO = np.sqrt(252) * average / downside_vol if (downside_vol > 0 or not np.isnan(downside_vol)) else np.nan
+        SHARPE = np.sqrt(252) * average / vol if (vol > 0 and not np.isnan(vol)) else np.nan
+        SORTINO = np.sqrt(252) * average / downside_vol if (downside_vol > 0 and not np.isnan(downside_vol)) else np.nan
         VOLATILITY = vol * np.sqrt(252) if (vol > 0 or not np.isnan(vol)) else np.nan
         AVERAGE = average
         TOTAL = np.prod(1 + returns) - 1
@@ -99,5 +111,4 @@ class Backtester():
         ]
         values = [VOLATILITY, AVERAGE, TOTAL, CAGR, MAX_DD, VAR, CVAR]
         results = [round(SHARPE, 5), round(SORTINO, 5), round(CALMAR, 5)] + [round(x*100, 5) for x in values] + [round(SKEW, 5), round(KURTOSIS, 5), round(OMEGA, 5)]
-
         return dict(zip(performance_metrics, results))
--- opes/utils.py
+++ opes/utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.stats as scistats
 import pandas as pd
 
 from opes.errors import DataError, PortfolioError
